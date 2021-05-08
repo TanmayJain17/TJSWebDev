1ac211768d3bd8ddc40b16c281a508c6fce0a4ad
Tanmay <tanmay.jain260@gmail.com>
2021-05-08 22:34:02 +0530

    express-sql-api

---
+++ Webinar13/express-api-mysql2/routes/api.js
@@ -0,0 +1,23 @@
+const route = require('express').Router();
+const db = require('./dblink')
+
+route.get('/',(req, res) =>{
+    db.getAllUsers()
+    .then((persons)=>{
+        res.send({persons})
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+})
+
+route.post('/',(req, res) =>{
+    db.addNewPerson(req.body.name,req.body.age,req.body.city)
+    .then(()=>{
+        res.end()
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+})
+module.exports = {route}
---
+++ Webinar13/express-api-mysql2/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const api = require('./routes/api.js')
+const dblink = require('./routes/dblink.js')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true}))
+app.use('/public',express.static(__dirname + '/public/table.html'))
+
+app.use('/api',api)
+app.use('/dblink',dblink)
+
+
+app.listen(4244,()=>{
+    console.log('server started on http://localhost:4233')
+})
