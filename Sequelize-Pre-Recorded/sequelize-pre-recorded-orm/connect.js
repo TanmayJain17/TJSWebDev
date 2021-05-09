294d9d4e1f1154fbda3e484d80599ebf9ac399db
Tanmay <tanmay.jain260@gmail.com>
2021-05-09 18:32:13 +0530

    modified

--- Sequelize-Pre-Recorded/sequelize-pre-recorded-orm/connect.js
+++ Sequelize-Pre-Recorded/sequelize-pre-recorded-orm/connect.js
@@ -9,6 +9,6 @@ const db = new Sequelize('mysampledb','mysampleuser','mysamplepass',{
   .then(() => {
     console.log('Connection has been established successfully.');
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
