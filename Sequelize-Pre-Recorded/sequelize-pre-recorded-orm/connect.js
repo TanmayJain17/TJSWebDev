67632fad16f08a7b80d7811fecd2621ef79372c6
Tanmay <tanmay.jain260@gmail.com>
2021-05-09 15:20:00 +0530

    connection-established

---
+++ Sequelize-Pre-Recorded/sequelize-pre-recorded-orm/connect.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+const db = new Sequelize('mysampledb','mysampleuser','mysamplepass',{
+    host: 'localhost',
+    dialect:'mysql'
+})
+
+ //returns promise
+  db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
