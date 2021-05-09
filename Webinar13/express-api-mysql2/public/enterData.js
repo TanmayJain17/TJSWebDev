1638bb301a3cd7330dfec24fcd6696e95c9867da
Tanmay <tanmay.jain260@gmail.com>
2021-05-09 14:32:56 +0530

    public-folder-updated

--- Webinar13/express-api-mysql2/public/enterData.js
+++ Webinar13/express-api-mysql2/public/enterData.js
@@ -13,7 +13,13 @@ $(document).ready(
 
                 $.post('/api',{name:nameVal, age:ageVal, city:cityVal},(data)=>{
                     console.log('entered data into the database')
+
                 })
+                let msg = "Data Entered ✔️";
+                $(".success-messages").text(msg).fadeIn().delay(2000).fadeOut("slow");
+                $('h1').siblings($('<a>Check Details</a>').attr('href','http://localhost:4244/public'))
+
+                
 
             }
         )
--- Webinar13/express-api-mysql2/public/table.js
+++ Webinar13/express-api-mysql2/public/table.js
@@ -3,13 +3,26 @@ $(document).ready(
         let table = $('#tableData');
         table.empty();
         $.get('/api',(data)=>{
+            let mainFeildes=table.append($('<tr></tr>'));
+            mainFeildes.append($('<th>Name</th>'));
+            mainFeildes.append($('<th>Age</th>'));
+            mainFeildes.append($('<th>City</th>'));
+            console.log(data)
 
-            for(let person of data){
+
+            for(let i=0;i<data.length;i++){
+                let tableRow = table.append($('<tr></tr>'))
+                tableRow.append($('<td>' + data[i].name + '</td>'))
+                tableRow.append($('<td>' + data[i].age + '</td>'))
+                tableRow.append($('<td>' + data[i].city + '</td>'))
+            }
+
+            /*for(let person of data){
                 let tableRow = table.append($('<tr></tr>'))
                 tableRow.append($('<td>' + person.name + '</td>'))
                 tableRow.append($('<td>' + person.age + '</td>'))
                 tableRow.append($('<td>' + person.city + '</td>'))
-            }
+            }*/
             
             
         })
