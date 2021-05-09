666b917a7ff137bc7a1ece94e95274a66ee89377
Tanmay <tanmay.jain260@gmail.com>
2021-05-09 14:34:08 +0530

    updated-server-removed -errors

--- Webinar13/express-api-mysql2/server.js
+++ Webinar13/express-api-mysql2/server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 const api = require('./routes/api.js')
-const dblink = require('./routes/dblink.js')
+//const dblink = require('./routes/dblink.js')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:true}))
-app.use('/public',express.static(__dirname + '/public/table.html'))
+app.use('/public',express.static(__dirname + '/public'))
 
 app.use('/api',api)
-app.use('/dblink',dblink)
+//app.use('/dblink',dblink)
 
 
 app.listen(4244,()=>{
-    console.log('server started on http://localhost:4233')
+    console.log('server started on http://localhost:4244/public')
 })
